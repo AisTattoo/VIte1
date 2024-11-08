105396343ef384691d6b282eb3485d934e7eed5c
AisTattoo <adilmubarak126@gmail.com>
2024-11-08 22:14:50 +0700

    first commit

---
+++ adilproject/src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <div className="text-3xl font-bold underline flex justify-center bg-slate-400 min-h-screen items-center ">
+      <div>
+        <button
+         className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+         type="submit"
+        >
+          Belanja Sekarang
+        </button>
+      </div>
+    </div>
+  )
+}
